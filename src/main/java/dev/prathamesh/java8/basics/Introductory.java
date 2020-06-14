26123ca53afa7739a6e2e41e83aee889f2d49773
devprathamesh <bandivadekar.prathamesh@gmail.com>
2020-06-14 20:30:21 +0530

    Non-Essential Commit

---
+++ src/main/java/dev/prathamesh/java8/basics/Introductory.java
@@ -0,0 +1,29 @@
+package dev.prathamesh.java8.basics;
+
+import lombok.extern.log4j.Log4j;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@Log4j
+public class Introductory {
+    public static void main(String[] args) {
+
+        int sum = 0;
+
+        for(int i = 0; i <=100;i++){
+            sum += i;
+        }
+
+        System.out.printf("\n Sum Imperative Style : " + sum);
+
+        System.out.printf("\n Sum Declarative Style : " + IntStream.rangeClosed(0, 100).sum());
+
+        List<Integer> intList = Arrays.asList(0, 1, 2, 2, 3, 4, 5, 6, 6);
+
+        System.out.printf("\n Distinct List Declarative Style : %s", intList.stream().distinct().collect(Collectors.toList()));
+
+    }
+}
