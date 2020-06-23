66212522abff294c9cfe93a39232277bb3a91e49
Bandivadekar <Prathamesh.Bandivadekar@mastercard.com>
2020-06-23 19:03:48 +0530

    Checkin for Stream reduce()

---
+++ src/main/java/dev/prathamesh/java8/streams/StreamReduceExample.java
@@ -0,0 +1,27 @@
+package dev.prathamesh.java8.streams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class StreamReduceExample {
+	
+	private static List<Integer> integerList = Arrays.asList(23, 34, 87, 76, 109, 134, 250);
+	
+	private static Integer calculateSumWithSeed() {
+		return integerList.stream().reduce(0, (inputA, inputB) -> inputA + inputB);
+	}
+	
+	private static Optional<Integer> calculateSumWithoutSeed() {
+		return integerList.stream().reduce((inputA, inputB) -> inputA + inputB);
+	}	
+
+	public static void main(String[] args) {
+		System.out.printf("Result of Reduce Operation(Addition) with seed : %d", calculateSumWithSeed());
+		
+		System.out.printf("\nResult of Reduce Operation(Addition) without Seed : %s", calculateSumWithoutSeed());
+		
+		if(calculateSumWithoutSeed().isPresent())
+			System.out.println("\n Fetch result from Optional : " + calculateSumWithoutSeed().get());
+	}
+}
